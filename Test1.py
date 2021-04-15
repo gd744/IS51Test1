53494046c2e18d97d3aebd686bc82c8fd25b0b66
gd744 <gonzalezdavid7761@gmail.com>
2021-04-15 14:50:14 -0700

    Finish psuedocode

---
+++ Test1.py
@@ -0,0 +1,31 @@
+"""
+This programe will determine which salary payment option is superior. 
+OPTION 1 is a payment of 100 dollars per day for 10 days. 
+OPTION 2 starts at a dollar per day, but doubles each day for 10 days. 
+Two different functions will calculate each option.
+The first function will calculate OPTION1, multiplying 100 dollars * 10 days.
+The second option will calculate OPTION2, using 10 loops to calulate the doubling scaling wage. 
+If OPTION1 and OPTION2 are equal, user output will read "Both Options pay the same"
+If OPTION 1 is greater, output will read "Option1 pays more"
+If OPTION 2 is greater output will read "Option2 pays more"
+"""
+#PSUEDOCODE
+# Option1
+#     return 100 * 10
+# Option 2 
+#     amount = 1
+#     list1 = []
+#     loops 10 times
+#         adds amount to list1
+#         amount doubles, *=2
+#         reads = sum of all items in loop
+#     return sum
+# main()
+#     var1 = Option1
+#     var2 = Option2
+#     if var1 = var2
+#         print "Both options pay the same."
+#     if var1 < var2
+#         print "Option 2 pays more"
+#     else
+#         "Option 1 pays more"

1b3e4698f3bf7cb53e75ad76c1709830c326259e
gd744 <gonzalezdavid7761@gmail.com>
2021-04-15 14:47:01 -0700

    structuredenglish

---
+++ Test1.py
@@ -0,0 +1,11 @@
+"""
+This programe will determine which salary payment option is superior. 
+OPTION 1 is a payment of 100 dollars per day for 10 days. 
+OPTION 2 starts at a dollar per day, but doubles each day for 10 days. 
+Two different functions will calculate each option.
+The first function will calculate OPTION1, multiplying 100 dollars * 10 days.
+The second option will calculate OPTION2, using 10 loops to calulate the doubling scaling wage. 
+If OPTION1 and OPTION2 are equal, user output will read "Both Options pay the same"
+If OPTION 1 is greater, output will read "Option1 pays more"
+If OPTION 2 is greatm output will read "Option2 pays more"
+"""
